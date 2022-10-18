2d3c9ed018b8d81e98e826c69318b701b673f23b
Adalberto R. Ribeiro <beto1821@uol.com.br>
2022-10-18 15:28:06 -0300

    req01

---
+++ src/Races/Race.ts
@@ -0,0 +1,15 @@
+export default abstract class Race {
+  private readonly _name: string;
+  private readonly _dexterity: number;
+ 
+  constructor(name: string, dexterity: number) {
+    this._name = name;
+    this._dexterity = dexterity;
+  }
+
+  static createdRacesInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  public abstract maxLifePoints(): number;
+}
