6f718cdd872cdb5d6aed568a20b221662d5732f9
Adalberto R. Ribeiro <beto1821@uol.com.br>
2022-10-18 18:31:10 -0300

    req02

--- src/Races/Dwarf.ts
+++ src/Races/Dwarf.ts
@@ -2,13 +2,19 @@ import Race from './Race';
 
 export default class Dwarf extends Race {
   private _maxlifePoints: number;
+  static racesInstances = 0;
 
   constructor(name: string, dexterity: number) {
     super(name, dexterity);
     this._maxlifePoints = 80;
+    Dwarf.racesInstances += 1;
   }
 
   get maxLifePoints(): number {
     return this._maxlifePoints;
+  }
+
+  static createdRacesInstances(): number {
+    return Dwarf.racesInstances;
   }
 }
--- src/Races/Orc.ts
+++ src/Races/Orc.ts
@@ -2,13 +2,19 @@ import Race from './Race';
 
 export default class Orc extends Race {
   private _maxlifePoints: number;
+  static racesInstances = 0;
 
   constructor(name: string, dexterity: number) {
     super(name, dexterity);
     this._maxlifePoints = 74;
+    Orc.racesInstances += 1;
   }
 
   get maxLifePoints(): number {
     return this._maxlifePoints;
+  }
+
+  static createdRacesInstances(): number {
+    return Orc.racesInstances;
   }
 }
