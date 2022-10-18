7a76523049c9d5fdbe76458347a4d7b6ec2ad056
Adalberto R. Ribeiro <beto1821@uol.com.br>
2022-10-18 15:32:37 -0300

    req01

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -1,10 +1,18 @@
 export default abstract class Race {
   private readonly _name: string;
   private readonly _dexterity: number;
- 
+
   constructor(name: string, dexterity: number) {
     this._name = name;
     this._dexterity = dexterity;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get dexterity(): number {
+    return this._dexterity;
   }
 
   static createdRacesInstances(): number {
